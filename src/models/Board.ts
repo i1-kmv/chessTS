2d8983aab6a46b7ee382f23e325aea0684d7ba0f
i1-kmv <i1-kmv@mail.ru>
2022-05-28 20:23:09 +0300

    add figures on board

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,5 +1,11 @@
+import { Knight } from './figure/Knight';
 import { Colors } from './Colors';
 import { Cell } from "./Cell";
+import { Queen } from './figure/Queen';
+import { Pawn } from './figure/Pawn';
+import { King } from './figure/King';
+import { Rook } from './figure/Rook';
+import { Bishop } from './figure/Bishop';
 
 export class Board { 
     cells: Cell[][] = [ ]
@@ -22,7 +28,50 @@ export class Board {
         return this.cells[y][x]
     }
 
-    public addFigures() {
+    private addPawns() {
+        for (let i = 0; i < 8; i++) {
+            new Pawn(Colors.BLACK, this.getCell(i, 1))
+            new Pawn(Colors.WHITE, this.getCell(i, 6))
+        }
+    }
 
+    private addKings () {
+       new King(Colors.BLACK, this.getCell(4, 0))
+       new King(Colors.WHITE, this.getCell(4, 7))
+    }
+
+    private addQueens() {
+        new Queen(Colors.BLACK, this.getCell(3, 0))
+        new Queen(Colors.WHITE, this.getCell(3, 7))
+    }
+
+    private addRooks() {
+        new Rook(Colors.BLACK, this.getCell(0, 0))
+        new Rook(Colors.BLACK, this.getCell(7, 0))
+        new Rook(Colors.WHITE, this.getCell(0, 7))
+        new Rook(Colors.WHITE, this.getCell(7, 7))
+    }
+
+    private addBishops() {
+        new Bishop(Colors.BLACK, this.getCell(2, 0))
+        new Bishop(Colors.BLACK, this.getCell(5, 0))
+        new Bishop(Colors.WHITE, this.getCell(2, 7))
+        new Bishop(Colors.WHITE, this.getCell(5, 7))
+    }
+
+    private addKnights() {
+        new Knight(Colors.BLACK, this.getCell(1, 0))
+        new Knight(Colors.BLACK, this.getCell(6, 0))
+        new Knight(Colors.WHITE, this.getCell(1, 7))
+        new Knight(Colors.WHITE, this.getCell(6, 7))
+    }
+
+    public addFigures() {
+        this.addPawns()
+        this.addBishops()
+        this.addKings()
+        this.addKnights()
+        this.addQueens()
+        this.addRooks()
     }
 }
