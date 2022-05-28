b43670a9a25ba77133290d711111ab43f04a009b
i1-kmv <i1-kmv@mail.ru>
2022-05-28 19:57:42 +0300

    create figures

--- src/models/figure/Figure.ts
+++ src/models/figure/Figure.ts
@@ -1,3 +1,30 @@
-export class Figure {
+import { Cell } from './../Cell';
+import { Colors } from "../Colors";
+import logo from '../../assets/black-king.png'
 
+export enum FigureNames {
+    FIGURE = "Фигура",
+    KING = "Король",
+    KNIGHT = "Конь",
+    PAWN = "Пешка",
+    QUEEN = "Ферзь",
+    ROOK = "Ладья",
+    BISHOP = "Слон",
+}
+
+export class Figure {
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: number;
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureNames.FIGURE;
+        this.id = Math.random();
+    }
 }
